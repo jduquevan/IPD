Add history aggregator. Extra-gradient bug fix.

src/agents.py
@@ -8,7 +8,7 @@ from functools import reduce
 
 from .algos import get_reset_history
 from .ipd import IPD
-from .models import DRLActor
+from .models import DRLActor, HistoryAggregator
 from .optimizers import ExtraAdam
 
 class BaseAgent():
@@ -49,6 +49,8 @@ class VIPAgent(BaseAgent):
                  num_rollouts,
                  rollout_len,
                  communication_len,
+                 representation_size,
+                 history_len,
                  device,
                  n_actions,
                  history_size,
@@ -64,38 +66,71 @@ class VIPAgent(BaseAgent):
         self.num_rollouts = num_rollouts
         self.rollout_len = rollout_len
         self.communication_len = communication_len
+        self.representation_size =representation_size
+        self.history_len = history_len
         self.n_actions = n_actions
         self.history_size = history_size
         self.transition: list = list()
+        self.history = []
+        self.rollout_history = []
 
-        self.actor = DRLActor(in_size=self.obs_size + history_size + n_actions,
+        self.history_aggregator = HistoryAggregator(in_size=self.obs_size + 2*n_actions,
+                                                    out_size=self.representation_size,
+                                                    device=self.device,
+                                                    hidden_size=self.hidden_size)
+        self.actor = DRLActor(in_size=self.obs_size + representation_size + n_actions,
                               out_size=self.n_actions,
                               device=self.device,
                               hidden_size=self.hidden_size)
+        self.history_aggregator.to(self.device)
         self.actor.to(self.device)
         self.model = model
 
         if self.opt_type.lower() == "sgd":
-            self.optimizer = optim.SGD(self.actor.parameters(), 
+            self.optimizer = optim.SGD(list(self.actor.parameters()) + 
+                                       list(self.history_aggregator.parameters()), 
                                        lr=optim_config["lr"],
                                        momentum=optim_config["momentum"],
                                        weight_decay=optim_config["weight_decay"],
                                        maximize=True)
         elif self.opt_type.lower() == "adam":
-            self.optimizer = optim.Adam(self.actor.parameters(), 
+            self.optimizer = optim.Adam(list(self.actor.parameters()) + 
+                                        list(self.history_aggregator.parameters()), 
                                         lr=optim_config["lr"],
                                         weight_decay=optim_config["weight_decay"],
                                         maximize=True)
         elif self.opt_type.lower() == "eg":
-            self.optimizer = ExtraAdam(self.actor.parameters(), 
+            self.optimizer = ExtraAdam(list(self.actor.parameters()) + 
+                                       list(self.history_aggregator.parameters()),
                                        lr=optim_config["lr"],
                                        betas=(optim_config["beta_1"], optim_config["beta_2"]),
                                        weight_decay=optim_config["weight_decay"])
+    
+    def reset_history(self):
+        self.history = []
 
-    def select_action(self, state, agent=None, dist_b=None):
+    def set_history(self, history):
+        self.history = history
+
+    def update_history(self, a, b, obs):
+        act_obs = torch.cat([a, b, obs.flatten()])
+        self.history.append(act_obs)
+        self.history = self.history[-self.history_len:]
+
+    def aggregate_history(self):
+        if self.history:
+            history_tensor = torch.cat(self.history).to(self.device).reshape((1, len(self.history), -1))
+            history = self.history_aggregator(history_tensor)
+        else:
+            history = -1 * torch.ones(self.representation_size).to(self.device)
+        return history.flatten()
+
+    def select_action(self, state, agent=None, dist_b=None, state_b=None):
         self.steps_done += 1
         h_0, dist_a = self.actor(state, dist_b)
-        if not agent is None:
+        if not agent is None and not state_b is None:
+            dist_a, dist_b = self.unroll_policies(state, h_0, dist_a, agent, state_b)
+        elif not agent is None:
             dist_a, dist_b = self.unroll_policies(state, h_0, dist_a, agent)
         index = torch.tensor([np.random.choice(self.n_actions, p=dist_a.cpu().detach().numpy())],
                               requires_grad=False,
@@ -104,11 +139,14 @@ class VIPAgent(BaseAgent):
         action = action.scatter(0, index, 1)
         return action
 
-    def unroll_policies(self, state, h_0, dist_a, agent):
+    def unroll_policies(self, state, h_0, dist_a, agent, state_b=None):
         j_0, dist_b = agent.actor(state, dist_a)
         for i in range(self.communication_len):
             h_0, dist_a = self.actor(state, dist_b, h_0)
-            j_0, dist_b = agent.actor(state, dist_a, j_0)
+            if not state_b is None:
+                j_0, dist_b = agent.actor(state_b, dist_a, j_0)
+            else:
+                j_0, dist_b = agent.actor(state, dist_a, j_0)
         return dist_a, dist_b
 
     def compute_value(self, agent, env_type="ipd", communication=True):
@@ -122,16 +160,42 @@ class VIPAgent(BaseAgent):
             steps = self.cum_steps
             t_rewards = []
             log_probs = []
-            obs, history = self.transition
-            state = torch.cat([obs.flatten(), history])
+            if env_type == "ipd":
+                obs, history = self.transition
+            elif env_type == "cg":
+                obs, history_a, hist_a = self.transition
+                _, history_b, hist_b = agent.transition
 
             for j in range(self.rollout_len):
-                h_0, dist_a = self.actor(state)
-                if communication:
-                    dist_a, dist_b = self.unroll_policies(state, h_0, dist_a, agent)
-                else:
-                    j_0, dist_b = agent.actor(state, dist_a)
-                    h_0, dist_a = self.actor(state, dist_b)
+                if env_type == "ipd":
+                    state = torch.cat([obs.flatten(), history])
+                elif env_type == "cg":
+                    state_a = torch.cat([obs.flatten(), history_a])
+                    state_b = torch.cat([obs.flatten(), history_b])
+
+                if steps % self.steps_reset == 0 and env_type == "ipd":
+                    history = get_reset_history(env_type, self.device)
+                elif steps % self.steps_reset == 0 and env_type == "cg":
+                    self.reset_history()
+                    agent.reset_history()
+                    history_a = self.aggregate_history()
+                    history_b = agent.aggregate_history()
+
+                if env_type == "ipd":
+                    h_0, dist_a = self.actor(state)
+                    if communication:
+                        dist_a, dist_b = self.unroll_policies(state, h_0, dist_a, agent)
+                    else:
+                        j_0, dist_b = agent.actor(state, dist_a)
+                        h_0, dist_a = self.actor(state, dist_b)
+                elif env_type == "cg":
+                    h_0, dist_a = self.actor(state_a)
+                    if communication:
+                        dist_a, dist_b = self.unroll_policies(state_a, h_0, dist_a, agent, state_b)
+                    else:
+                        j_0, dist_b = agent.actor(state_b, dist_a)
+                        h_0, dist_a = self.actor(state_a, dist_b)
+
                 index_a = torch.tensor([np.random.choice(self.n_actions, p=dist_a.cpu().detach().numpy())],
                                        requires_grad=False,
                                        device=self.device)
@@ -150,15 +214,17 @@ class VIPAgent(BaseAgent):
                 log_probs.append(torch.log(a_t_prob))
                 log_probs.append(torch.log(b_t_prob))
 
+                last_obs = obs
                 obs, r1, r2, _, _, _  = self.model.step([action_a, action_b])
                 if env_type == "ipd":
                     history = torch.cat([action_a, action_b])
                 elif env_type == "cg":
-                    history = torch.cat([self.model.j1, self.model.j2])
+                    self.update_history(action_a, action_b, last_obs)
+                    agent.update_history(action_b, action_a, last_obs)
+                    history_a = self.aggregate_history()
+                    history_b = agent.aggregate_history()
 
                 t_rewards.append(r1)
-                if steps % self.steps_reset == 0:
-                    history = get_reset_history(env_type, self.device)
                 steps = steps + 1
 
             reward_t = torch.sum(torch.cat(t_rewards, dim=0))
@@ -166,7 +232,11 @@ class VIPAgent(BaseAgent):
 
             # Reinforce estimator
             estimated_rewards.append((reward_t.detach() * sum_log_probs).unsqueeze(dim=0))
-            
+
+        if env_type == "cg":
+            self.set_history(hist_a)
+            agent.set_history(hist_b)
+
         game_value = torch.sum(torch.cat(estimated_rewards, dim=0))/self.num_rollouts
 
         return game_value

src/algos.py
@@ -10,7 +10,7 @@ from typing import Any, Dict, Optional
 from .optimizers import ExtraAdam
 from .utils import WandbLogger
 
-def optimize_models(opt_type, opt_1, opt_2, loss_1, loss_2):
+def optimize_models(opt_type, opt_1, opt_2, loss_1, loss_2, t):
     if opt_type == "sgd" or opt_type == "adam":
         opt_1.zero_grad()
         opt_2.zero_grad()
@@ -23,18 +23,21 @@ def optimize_models(opt_type, opt_1, opt_2, loss_1, loss_2):
         loss_2 = -1 * loss_2 
         opt_1.zero_grad()
         opt_2.zero_grad()
-        loss_1.backward()
+        loss_1.backward(retain_graph=True)
         loss_2.backward()
-        opt_1.extrapolation()
-        opt_2.extrapolation()
-        opt_1.step()
-        opt_2.step()
+        if t % 2 == 0:
+            opt_1.extrapolation()
+            opt_2.extrapolation()
+        else:
+            opt_1.step()
+            opt_2.step()
 
-def get_reset_history(env, device):
+def get_reset_history(env, device, agent=None):
     if env == "ipd":
         return torch.tensor([-1, -1, -1, -1], device=device)
     elif env == "cg":
-        return torch.tensor([-1, -1], device=device)
+        agent.reset_history()
+        return agent.aggregate_history()
 
 def evaluate_agents(agent_1, agent_2, evaluation_steps, eval_env, env_type, device):
     # TODO: Implement always cooperate and defect agents for IPD and CG
@@ -104,29 +107,46 @@ def run_vip(env,
 
     for i_episode in range(num_episodes):
         obs, _ = env.reset()
-        history = get_reset_history(env_type, device)
-        for t in count():
-            if t % steps_reset == 0:
-                history = get_reset_history(env_type, device)
-            agent_1.transition = [obs, history]
-            agent_2.transition = [obs, history]
 
-            state = torch.cat([obs.flatten(), history])
-            action_1 = agent_1.select_action(state, agent_2)
-            action_2 = agent_2.select_action(state, agent_1)
+        if env_type == "ipd":
+            history = get_reset_history(env_type, device)
+        elif env_type == "cg":
+            history_1 = get_reset_history(env_type, device, agent_1)
+            history_2 = get_reset_history(env_type, device, agent_2)
+        
+        for t in count():
+            if t % steps_reset == 0 and env_type == "ipd":
+                history = get_reset_history(env_type, device)
+            elif t % steps_reset == 0 and env_type == "cg":
+                history_1 = get_reset_history(env_type, device, agent_1)
+                history_2 = get_reset_history(env_type, device, agent_2)
+
+            if env_type == "ipd":
+                agent_1.transition = [obs, history]
+                agent_2.transition = [obs, history]
+                state = torch.cat([obs.flatten(), history])
+                action_1 = agent_1.select_action(state, agent_2)
+                action_2 = agent_2.select_action(state, agent_1)
+            elif env_type == "cg":
+                agent_1.transition = [obs, history_1, agent_1.history]
+                agent_2.transition = [obs, history_2, agent_2.history]
+                state_1 = torch.cat([obs.flatten(), history_1])
+                state_2 = torch.cat([obs.flatten(), history_2])
+                action_1 = agent_1.select_action(state_1, agent_2, state_b=state_2)
+                action_2 = agent_2.select_action(state_2, agent_1, state_b=state_1)
             
+            last_obs = obs
             obs, r1, r2, _, _, _  = env.step([action_1, action_2])
 
             if env_type == "ipd":
                 history = torch.cat([action_1, action_2])
             elif env_type == "cg":
-                agent_1.model.j1 = env.j1
-                agent_1.model.j2 = env.j2
-                agent_2.model.j1 = env.j1
-                agent_2.model.j2 = env.j2
-                history = torch.cat([env.j1, env.j2])
+                agent_1.update_history(action_1, action_2, last_obs)
+                agent_2.update_history(action_2, action_1, last_obs)
+                history_1 = agent_1.aggregate_history()
+                history_2 = agent_2.aggregate_history()
 
-            if t % 2 == 0:
+            if t % 4 == 0 or t % 4 == 1:
                 value_1 = agent_1.compute_value(agent_2, env_type=env_type)
                 value_2 = agent_2.compute_value(agent_1, env_type=env_type)
             else:
@@ -137,7 +157,8 @@ def run_vip(env,
                             agent_1.optimizer, 
                             agent_2.optimizer,
                             value_1,
-                            value_2)
+                            value_2,
+                            t)
 
             d_1, c_1, d_2, c_2 = None, None, None, None
             if t % evaluate_every == 0 and env_type == "ipd":